33a57d1123f7c0f409381ec2842411849a85ad4b
Vlad <vladik.gutu@gmail.com>
2024-12-06 18:42:18 +0200

    +

--- Starter.py
+++ Starter.py
@@ -1,7 +1,7 @@
 import asyncio
 
 from aiogram import Bot, Dispatcher, types
-from aiogram.filters import CommandStart, Command
+from aiogram.filters import Command
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, WebAppInfo, ReplyKeyboardMarkup
 
 from Config import API_TOKEN, MINI_APP_URL
@@ -9,19 +9,13 @@ from Config import API_TOKEN, MINI_APP_URL
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
-
-def get_webapp_button():
-    keyboard = InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="Open App", web_app=WebAppInfo(url=MINI_APP_URL))]
-    ])
-    return keyboard
-
 @dp.message(Command("start"))
 async def start(message: types.Message):
     await message.answer(
         "Привет! Нажми на кнопку ниже, чтобы открыть веб-приложение.",
-        reply_markup=get_webapp_button()
-    )
+        reply_markup=InlineKeyboardMarkup(inline_keyboard=[
+            [InlineKeyboardButton(text="Open App", web_app=WebAppInfo(url=MINI_APP_URL))]]))
+
 
 async def main():
     await dp.start_polling(bot)
