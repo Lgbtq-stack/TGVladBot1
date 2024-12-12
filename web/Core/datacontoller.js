cc5b6741b70fbe585cc5b7470d7dabfa97be2663
Nichita228 <67234692+Nichita228@users.noreply.github.com>
2024-12-12 19:26:52 +0200

    Add Backend

---
+++ web/Core/datacontoller.js
@@ -0,0 +1,27 @@
+
+//format
+// {
+//     "wallet":"GDTOJL273O5YKNF3PIG72UZRG6CT4TRLDQK2NT5ZBMN3A56IP4JSYRUQ",
+//     "tokens": {
+//     "BTC": {
+//         "balance": 1.25,
+//             "history": {
+//             "2024-12-10T13:37:50.124Z": 0.25,
+//                 "2024-12-11T13:37:50.124Z": 0.5,
+//                 "2024-12-12T13:37:50.124Z": 0.5
+//         },
+//         "time_to_mine": "2024-12-12T20:00:00Z"
+//     }
+// }
+// }
+
+export async function get_config(user_id) {
+    const wallet_data = await getActiveWallet(user_id);
+
+    const config = {
+        "wallet": wallet_data.wallet,
+        "tokens": {
+
+        }
+    }
+}
