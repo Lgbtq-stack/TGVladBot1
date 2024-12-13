6f674e7ecabeb36f2fe277f445cbff54e4027407
Nichita228 <67234692+Nichita228@users.noreply.github.com>
2024-12-13 03:02:00 +0200

    Finish Backend

---
+++ web/Core/database.js
@@ -0,0 +1,13 @@
+export async function getActiveWallet(user_id) {
+    try {
+        const response = await fetch(`http://162.33.177.26:3000/api/wallets/active/${user_id}`);
+        if (!response.ok) {
+            throw new Error(`Error: ${response.statusText}`);
+        }
+        return await response.json();
+    } catch (error) {
+        console.error('Error getting wallets:', error.message);
+        throw error;
+    }
+}
+
