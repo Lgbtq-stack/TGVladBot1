24092e94f7de3756cc556e2d10eae1444f21b1fe
Nichita228 <126054987+NichitaC12@users.noreply.github.com>
2024-12-14 15:29:15 +0200

    asd

--- web/Core/database.js
+++ web/Core/database.js
@@ -1,5 +1,6 @@
 export async function getActiveWallet(user_id) {
     try {
+        console.log('Getting active wallet for user', user_id);
         const response = await fetch(`https://miniappserv.com/api/wallets/active/${user_id}`);
         if (!response.ok) {
             throw new Error(`Error: ${response.statusText}`);
